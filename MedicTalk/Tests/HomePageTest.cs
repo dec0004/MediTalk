9bf6ee507aeae17b9b81e967129ae33684282cf1
unknown <Jordandecarheil1992@gmail.com>
2018-05-20 18:49:46 +1000

    Added more tests

--- MedicTalk/Tests/HomePageTest.cs
+++ MedicTalk/Tests/HomePageTest.cs
@@ -53,22 +53,41 @@ namespace MediTalk
 			Assert.AreEqual(_home.form1.Username_Textbox.Text, "");
 			Assert.AreEqual(_home.form1.Password_Textbox.Text, "");
 		}
-		/*
+		
 		[Test]
 		public void Test_Emergency_Request_Works()
 		{
 			_form1 = new Form1();
 			_mysql = new Mysql_Connect();
-			_form1.UserIDProperty = "5";
-			_form1.FirstNameProperty = "resident";
-			_form1.LastNameProperty = "Burii";
-		
-	
+			_form1.Username_Textbox.Text = "resident";
+			_form1.Password_Textbox.Text = "resident";
+			_form1.UserNameProperty = "resident";
+			_form1.PasswordProperty = "resident";
+			_form1.Login_Click(new object(), EventArgs.Empty);
+
+
 			_home = new HomePage(_form1, _mysql);
 			_home.button3_Click(new object(), EventArgs.Empty);
 
 			Assert.AreNotEqual(_home._status, "success");
 		}
-		*/
+		
+
+		[Test]
+		public void Test_General_Assistance()
+		{
+			_form1 = new Form1();
+			_mysql = new Mysql_Connect();
+			_home = new HomePage(_form1, _mysql);
+			
+			_form1.Username_Textbox.Text = "resident";
+			_form1.Password_Textbox.Text = "resident";
+			_form1.UserNameProperty = "resident";
+			_form1.PasswordProperty = "resident";
+			_form1.Login_Click(new object(), EventArgs.Empty);
+
+			_home.button3_Click(new object(), EventArgs.Empty);
+
+		}
 	}
 }
